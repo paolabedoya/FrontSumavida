da91cc5390247f190f3c468b9b531165ae07c10c
nachomglz <nachopad3@gmail.com>
2023-10-12 11:09:32 +0100

    Created project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import Header from "./components/Header/Header"
+// import { BrowserRouter, Outlet } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <div>
+      app
+    </div>
+  )
+}
+
+export default App
